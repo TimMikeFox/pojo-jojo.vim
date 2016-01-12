3c1ddef857637b176582f4fb267014f310ff8d60
Timothy Fox <tim.mike.fox@gmail.com>
2016-01-12 01:56:25 -0500

    initial pojojojo def

---
+++ ruby/lib/pojojojo.rb
@@ -0,0 +1,13 @@
+module PoJo
+    class JoJo
+
+        def initialize(className)
+            @className = className
+        end
+
+        def pojoize(fields)
+            lines = []
+            lines
+        end
+    end
+end
---
+++ ruby/spec/pojo_jojo_spec.rb
@@ -0,0 +1,8 @@
+require 'pojojojo'
+
+describe PoJo::JoJo, "#pojoize" do
+
+    before :each do
+        @pojoj = PoJo::JoJo.new "ClassName"
+    end
+end
