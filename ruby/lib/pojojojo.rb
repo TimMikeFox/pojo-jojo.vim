79e400d750123c2c2fe614f27a984fc80b0dabfa
Timothy Fox <tim.mike.fox@gmail.com>
2016-01-12 02:00:55 -0500

    one property with getters and setters

--- ruby/lib/pojojojo.rb
+++ ruby/lib/pojojojo.rb
@@ -8,8 +8,26 @@ module PoJo
         def pojoize(fields)
             lines = []
             lines << "public class #{@className} {"
+            write_properties lines, fields
+            lines << "" unless fields.empty?
+            write_getters_and_setters lines, fields
+            lines << "" unless fields.empty?
             lines << "}"
             lines
         end
+
+        def write_properties(lines, fields)
+            fields.each do |t, n|
+                lines << "private #{t} #{n};"
+            end
+        end
+        
+        def write_getters_and_setters(lines, fields)
+            fields.each do |t, n|
+                lines << "public #{t} get#{n[0].upcase}#{n[1..-1]}() { return #{n}; }"
+                lines << "public void set#{n[0].upcase}#{n[1..-1]}(#{t} #{n}) { this.#{n} = #{n}; }"
+            end
+        end
+
     end
 end
--- ruby/spec/pojo_jojo_spec.rb
+++ ruby/spec/pojo_jojo_spec.rb
@@ -13,4 +13,21 @@ describe PoJo::JoJo, "#pojoize" do
         end
     end
 
+    context "one field" do
+
+        input = [["String","theProperty"]]
+
+        it "should create a private property, getter, and setter" do
+            property = "private String theProperty;"
+            getter = "public String getTheProperty() { return theProperty; }"
+            setter = "public void setTheProperty(String theProperty) { this.theProperty = theProperty; }"
+
+            result = @pojoj.pojoize(input)
+
+            expect(result).to include(property)
+            expect(result).to include(getter)
+            expect(result).to include(setter)
+        end
+    end
+
 end
